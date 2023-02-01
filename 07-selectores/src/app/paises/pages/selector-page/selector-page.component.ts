945045e1bb8d97e70529ae53b41bfe9eecafe37d
Bruno Oliveira <brunodevoliveira@gmail.com>
2023-01-31 23:38:14 -0300

    Buscas por região e pais prontas

--- 07-selectores/src/app/paises/pages/selector-page/selector-page.component.ts
+++ 07-selectores/src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,9 @@
+import { Pais } from './../../interfaces/paises.interface';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { switchMap, tap } from 'rxjs';
+import { PaisSmall } from '../../interfaces/paises.interface';
+import { PaisesService } from '../../services/paises-service.service';
 
 @Component({
   selector: 'selector-page',
@@ -9,11 +13,58 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SelectorPageComponent implements OnInit {
   public formulario: FormGroup = this.fb.group({
     regiao: ['', Validators.required],
+    pais: ['', Validators.required],
+    fronteira: ['', Validators.required],
   });
 
-  constructor(private fb: FormBuilder) {}
+  regioes: string[] = [];
+  paises: PaisSmall[] = [];
+  fronteiras: string[] = [];
+  exibirFronteiras = true;
 
-  ngOnInit(): void {}
+  constructor(private fb: FormBuilder, private paiseServce: PaisesService) {}
+
+  ngOnInit(): void {
+    this.regioes = this.paiseServce.regioes;
+
+    // Busca por regiao
+    // this.formulario.get('regiao')?.valueChanges.subscribe((regiao) => {
+    //   console.log(regiao);
+    //   this.paiseServce.buscarPaisesPorRegiao(regiao).subscribe((paises) => {
+    //     console.log(paises);
+
+    //     this.paises = paises;
+    //   });
+    // });
+
+    this.formulario
+      .get('regiao')
+      ?.valueChanges.pipe(
+        // Dispara um efeito secundário
+        tap((_) => {
+          this.formulario.get('pais')?.reset('');
+        }),
+        // Tenho o valor do observable retornado do select de regiao, com ele posso fazer a troca (switch) pelo observable que retorna os países
+        switchMap((regiao) => this.paiseServce.buscarPaisesPorRegiao(regiao))
+      )
+      .subscribe((paises) => {
+        this.paises = paises;
+      });
+
+    // Ao selecionar o Pais envio o alphCode que vem do HTML para uma nova petição
+    this.formulario
+      .get('pais')
+      ?.valueChanges.pipe(
+        tap((_) => {
+          this.fronteiras = [];
+          this.formulario.get('fronteira')?.reset('');
+        }),
+        switchMap((cod) => this.paiseServce.buscarPaisPorCodigo(cod))
+      )
+      .subscribe((pais) => {
+        this.fronteiras = pais?.borders || [];
+      });
+  }
 
   guardar() {
     console.log(this.formulario.value);
--- 07-selectores/src/app/paises/services/paises-service.service.ts
+++ 07-selectores/src/app/paises/services/paises-service.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Pais, PaisSmall } from '../interfaces/paises.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class PaisesServiceService {
+export class PaisesService {
+  private baseUrl: string = 'https://restcountries.com/v2';
 
-  constructor() { }
+  private _regioes: string[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
+  get regioes(): string[] {
+    return [...this._regioes]; //Dessa forma evito que passe o array por referência
+  }
+
+  constructor(private http: HttpClient) {}
+
+  buscarPaisesPorRegiao(regiao: string): Observable<PaisSmall[]> {
+    const url: string = `${this.baseUrl}/region/${regiao}?fields=alpha3Code,name`;
+    return this.http.get<PaisSmall[]>(url);
+  }
+
+  buscarPaisPorCodigo(codigo: string): Observable<Pais | null> {
+    if (!codigo) {
+      return of(null);
+    }
+    const url: string = `${this.baseUrl}/alpha/${codigo}`;
+    return this.http.get<Pais>(url);
+  }
 }
