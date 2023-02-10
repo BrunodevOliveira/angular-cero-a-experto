04feca720491c25927d5ade68a1be3a4d755b2fe
Bruno Oliveira <brunodevoliveira@gmail.com>
2023-02-10 19:40:52 -0300

    add método localeCompare no pipe ordenar-nome

--- 04-pipesApp/src/app/vendas/pipes/ordenar-nome.pipe.ts
+++ 04-pipesApp/src/app/vendas/pipes/ordenar-nome.pipe.ts
@@ -11,13 +11,14 @@ type listaDeHeroisOrdenada = {
 export class OrdenarNomePipe implements PipeTransform {
   transform(herois: Heroi[], ordenarPor: string = 'sem valor'): Heroi[] {
     const tipoDeOrdem: listaDeHeroisOrdenada = {
-      nome: () => herois.sort((a, b) => (a.nome > b.nome ? 1 : -1)),
+      nome: () => herois.sort((a, b) => a.nome.localeCompare(b.nome)),
       color: () => herois.sort((a, b) => (a.color > b.color ? 1 : -1)),
       voa: () => herois.sort((a, b) => (a.voa > b.voa ? -1 : 1)),
       'sem valor': () => herois,
     };
     return tipoDeOrdem[ordenarPor]();
 
+    //! "localeCompare()" é usado para comparar dois valores de string e determinar qual vem antes na ordem alfabética.
     // switch (ordenarPor) {
     //   case 'nome':
     //     return herois.sort((a, b) => (a.nome > b.nome ? 1 : -1));
