324b45a2744388888b3c4ed3f2c5a741341c671b
Bruno Oliveira <brunodevoliveira@gmail.com>
2023-05-04 22:13:59 -0300

    Seção 17: ReactiveForms - Formulários Dinamicos com FormArray

--- 06.1-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 06.1-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DynamicPageComponent {
+  private fb: FormBuilder = inject(FormBuilder);
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array(
+      [
+        ['Metal Gear', Validators.required],
+        ['Death Stranding', Validators.required],
+      ],
+      Validators.required
+    ),
+  });
+
+  public newFavorite: FormControl = new FormControl('', Validators.required);
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  onAddToFavorites(): void {
+    if (this.newFavorite.invalid) return;
+
+    const newGame = this.newFavorite.value;
+
+    // this.favoriteGames.push(new FormControl(newGame, Validators.required))
+    this.favoriteGames.push(this.fb.control(newGame, Validators.required));
+    this.newFavorite.reset();
+  }
+
+  onDeleteFavorite(index: number): void {
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched;
+      console.log('Form inválido!');
+      return;
+    }
+    console.log(this.myForm.value);
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([]);
+    this.myForm.reset();
+  }
 }
