76990b25ddb5f1de97bfdd7837051eb04a63d9aa
Bruno Oliveira <brunodevoliveira@gmail.com>
2023-05-06 19:15:58 -0300

    seção 20: LifeCycle Hooks

---
+++ 08.1-lifeCycle/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { ProductsModule } from './products/products.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ProductsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ 08.1-lifeCycle/src/app/products/components/price/price.component.ts
@@ -0,0 +1,35 @@
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'products-price',
+  templateUrl: './price.component.html',
+  styles: [],
+})
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() price: number = 0;
+
+  public interval$?: Subscription;
+
+  ngOnInit(): void {
+    console.log('Component Filho: ngOnInit');
+
+    this.interval$ = interval(1000).subscribe((value) => {
+      console.log(`Tick: ${value}`);
+    });
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Component Filho: ngOnChanges', { changes });
+  }
+  ngOnDestroy(): void {
+    console.log('Component Filho: ngOnDestroy');
+    this.interval$?.unsubscribe();
+  }
+}
